1d5519f63e80687a93be940d0ee0bdc0821fc1cf
Wagner Andrade <wsawebmaster@yahoo.com.br>
2023-10-12 16:08:58 -0300

    Aula02 - Integração com Banco de Dados em Memória

--- src/main/java/br/com/wsysteminformatica/todolist/user/UserController.java
+++ src/main/java/br/com/wsysteminformatica/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.wsysteminformatica.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,22 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
 
-  /**
-   * String (texto)
-   * Integer (int) números inteiros
-   * Double (double) Números 0.0000
-   * Float (float) Números 0.000
-   * char (A C )
-   * Date (data)
-   * void
-   */
-  /**
-   * Body
-  */
   @PostMapping
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.name);
-    System.out.println(userModel.username);
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if(user != null) {
+      // Mensagem de erro
+      // Status Code
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+    }
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
