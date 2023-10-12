89446e2b0894ab962a643e85113999ca55c8aa5a
Wagner Andrade <wsawebmaster@yahoo.com.br>
2023-10-12 20:41:07 -0300

    Aula03 - Implementando segurança nos dados do usuário

---
+++ src/main/java/br/com/wsysteminformatica/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,65 @@
+package br.com.wsysteminformatica.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.wsysteminformatica.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    var servletPath = request.getServletPath();
+
+    if (servletPath.equals("/tasks/")) {
+
+      // Pegar a autenticação (usuário e senha)
+      var authorization = request.getHeader("Authorization");
+
+      var authEncoded = authorization.substring("Basic".length()).trim();
+
+      byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+      var authString = new String(authDecode);
+
+      // ["wsawebmaster", "12345"]
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
+
+      // Validar usuário
+      var user = this.userRepository.findByUsername(username);
+      if (user == null) {
+        response.sendError(401);
+      } else {
+        // Validar senha
+        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+        if (passwordVerify.verified) {
+          filterChain.doFilter(request, response);
+        } else {
+          response.sendError(401);
+        }
+        // Segue
+
+      }
+    } else {
+      filterChain.doFilter(request, response);
+    }
+  }
+}
